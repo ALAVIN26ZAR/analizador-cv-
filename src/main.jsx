60b6e520c887fa91f93f4215c2c37cdbd49ae750
ALAVIN26ZAR <amilzar@yahoo.com.ar>
2026-06-11 12:57:49 +0000

    Set up Vite React project

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AnalizadorCV from '../analizador-cv.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AnalizadorCV />
+  </React.StrictMode>,
+);
